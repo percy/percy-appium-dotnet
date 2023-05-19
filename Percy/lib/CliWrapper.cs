c6f47f11b5b18981c5beb2da3e89cbc70c6c38ab
Pankaj Yadav <pankajofficial443@gmail.com>
2023-05-19 18:07:37 +0530

    Update major version (#50)

--- Percy/lib/CliWrapper.cs
+++ Percy/lib/CliWrapper.cs
@@ -18,7 +18,7 @@ namespace PercyIO.Appium
   {
     public static readonly string CLI_API =
     Environment.GetEnvironmentVariable("PERCY_CLI_API") ?? "http://localhost:5338";
-    public static readonly string CLIENT_INFO = "percy-appium-dotnet/1.2.4";
+    public static readonly string CLIENT_INFO = "percy-appium-dotnet/2.0.0";
     public static readonly string ENVIRONMENT_INFO = Regex.Replace(
         Regex.Replace(RuntimeInformation.FrameworkDescription, @"\s+", "-"),
         @"-([\d\.]+).*$", "/$1").Trim().ToLower();
