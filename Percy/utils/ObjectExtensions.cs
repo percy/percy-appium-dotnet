6eabd8378cf3a0c4e77c3736419b55a30880ef1f
prklm10 <pradumraj360@gmail.com>
2023-06-05 13:30:23 +0530

    lint changes

--- Percy/utils/ObjectExtensions.cs
+++ Percy/utils/ObjectExtensions.cs
@@ -1,17 +1,17 @@
 namespace PercyIO.Appium
-{public static class ObjectExtensions
 {
+  public static class ObjectExtensions
+  {
     public static bool IsTrue(this object obj)
     {
-        string? str = obj?.ToString()?.ToLower();
-        return str == "true";
+      string? str = obj?.ToString()?.ToLower();
+      return str == "true";
     }
 
     public static bool IsFalse(this object obj)
     {
-        string? str = obj?.ToString()?.ToLower();
-        return str == "false";
+      string? str = obj?.ToString()?.ToLower();
+      return str == "false";
     }
-}
-
+  }
 }
