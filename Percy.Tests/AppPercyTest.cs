33a8c862754ad07adf419a1bb17e3904b4801801
prklm10 <pradumraj360@gmail.com>
2023-06-07 17:54:20 +0530

    refactoring tests

--- Percy.Tests/AppPercyTest.cs
+++ Percy.Tests/AppPercyTest.cs
@@ -10,22 +10,13 @@ namespace Percy.Tests
 {
   public class AppPercyTest
   {
+    private readonly MockDriverObject mockDriver;
+    private readonly StringWriter stringWriter;
 
-    private static String LogMessage(String label, String message, String color = "39m")
+    public AppPercyTest()
     {
-      return $"[\u001b[35m{label}\u001b[{color}] {message}";
-    }
-
-    [Fact]
-    public void TestName_WithAndroid_V4()
-    {
-      // Arrange
-      AppPercy.cache.Clear();
-      TestHelper.UnsetEnvVariables();
-      var mockDriver = new MockDriverObject();
+      mockDriver = new MockDriverObject();
       mockDriver.SessionId = "session-1";
-      mockDriver.SetCapability(MetadataBuilder.CapabilityBuilder("Android"));
-      mockDriver.setCommandExecutor("https://browserstack.com/wd/hub");
       CliWrapper.Healthcheck = () =>
       {
         return true;
@@ -42,9 +33,23 @@ namespace Percy.Tests
       mockHttp.When("http://localhost:5338/percy/comparison")
         .Respond("application/json", JsonConvert.SerializeObject(obj));
 
-      StringWriter stringWriter = new StringWriter();
+      stringWriter = new StringWriter();
       Console.SetOut(stringWriter);
+    }
 
+    private static String LogMessage(String label, String message, String color = "39m")
+    {
+      return $"[\u001b[35m{label}\u001b[{color}] {message}";
+    }
+
+    [Fact]
+    public void TestName_WithAndroid_V4()
+    {
+      // Arrange
+      AppPercy.cache.Clear();
+      TestHelper.UnsetEnvVariables();
+      mockDriver.SetCapability(MetadataBuilder.CapabilityBuilder("Android"));
+      mockDriver.setCommandExecutor("https://browserstack.com/wd/hub");
       // Act
       var name = "dummyName";
       AppPercy appPercy = new AppPercy(mockDriver);
@@ -62,31 +67,9 @@ namespace Percy.Tests
     {
       // Arrange
       AppPercy.cache.Clear();
-      TestHelper.UnsetEnvVariables();
-      var mockDriver = new MockDriverObject();
-      mockDriver.SessionId = "session-1";
       mockDriver.SetCapability(MetadataBuilder.CapabilityBuilder("iOS"));
       mockDriver.setCommandExecutor("https://browserstack.com/wd/hub");
-      CliWrapper.Healthcheck = () =>
-      {
-        return true;
-      };
-      var mockHttp = new MockHttpMessageHandler();
-      var expected = "https://percy.io/api/v1/comparisons/redirect?snapshot[name]=test%20screenshot&tag[name]=Samsung&tag[os_name]=Android&tag[os_version]=9&tag[width]=1280&tag[height]=1420&tag[orientation]=landscape";
-      var obj = new
-      {
-        success = true,
-        link = expected
-      };
-
-      CliWrapper.setHttpClient(new HttpClient(mockHttp));
-      // Setup a respond for the user api (including a wildcard in the URL)
-      mockHttp.When("http://localhost:5338/percy/comparison")
-        .Respond("application/json", JsonConvert.SerializeObject(obj));
-
-      StringWriter stringWriter = new StringWriter();
-      Console.SetOut(stringWriter);
-
+      
       // Act
       AppPercy appPercy = new AppPercy(mockDriver);
       var name = "dummyName";
@@ -105,30 +88,10 @@ namespace Percy.Tests
       // Arrange
       AppPercy.cache.Clear();
       TestHelper.UnsetEnvVariables();
-      var mockDriver = new MockDriverObject();
-      mockDriver.SessionId = "session-1";
-      mockDriver.SetCapability(MetadataBuilder.CapabilityBuilder("Android"));
       // Setting v5
       mockDriver.setIsV5(true);
+      mockDriver.SetCapability(MetadataBuilder.CapabilityBuilder("Android"));
       mockDriver.setCommandExecutor("https://browserstack.com/wd/hub");
-      CliWrapper.Healthcheck = () =>
-      {
-        return true;
-      };
-      var mockHttp = new MockHttpMessageHandler();
-      var expected = "https://percy.io/api/v1/comparisons/redirect?snapshot[name]=test%20screenshot&tag[name]=Samsung&tag[os_name]=Android&tag[os_version]=9&tag[width]=1280&tag[height]=1420&tag[orientation]=landscape";
-      var obj = new
-      {
-        success = true,
-        link = expected
-      };
-      CliWrapper.setHttpClient(new HttpClient(mockHttp));
-      // Setup a respond for the user api (including a wildcard in the URL)
-      mockHttp.When("http://localhost:5338/percy/comparison")
-        .Respond("application/json", JsonConvert.SerializeObject(obj));
-
-      StringWriter stringWriter = new StringWriter();
-      Console.SetOut(stringWriter);
 
       // Act
       var name = "dummyName";
@@ -148,31 +111,10 @@ namespace Percy.Tests
       // Arrange
       AppPercy.cache.Clear();
       TestHelper.UnsetEnvVariables();
-      var mockDriver = new MockDriverObject();
-      mockDriver.SessionId = "session-1";
-      mockDriver.SetCapability(MetadataBuilder.CapabilityBuilder("iOS"));
       // Setting v5
       mockDriver.setIsV5(true);
+      mockDriver.SetCapability(MetadataBuilder.CapabilityBuilder("iOS"));
       mockDriver.setCommandExecutor("https://browserstack.com/wd/hub");
-      CliWrapper.Healthcheck = () =>
-      {
-        return true;
-      };
-      var mockHttp = new MockHttpMessageHandler();
-      var expected = "https://percy.io/api/v1/comparisons/redirect?snapshot[name]=test%20screenshot&tag[name]=Samsung&tag[os_name]=Android&tag[os_version]=9&tag[width]=1280&tag[height]=1420&tag[orientation]=landscape";
-      var obj = new
-      {
-        success = true,
-        link = expected
-      };
-
-      CliWrapper.setHttpClient(new HttpClient(mockHttp));
-      // Setup a respond for the user api (including a wildcard in the URL)
-      mockHttp.When("http://localhost:5338/percy/comparison")
-        .Respond("application/json", JsonConvert.SerializeObject(obj));
-
-      StringWriter stringWriter = new StringWriter();
-      Console.SetOut(stringWriter);
 
       // Act
       AppPercy appPercy = new AppPercy(mockDriver);
