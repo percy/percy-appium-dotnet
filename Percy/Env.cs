3d0f59e65a6aef02ff1aeb191e06869183404fd1
prklm10 <pradumraj360@gmail.com>
2023-10-27 10:39:47 +0530

    releasing stable version

--- Percy/Env.cs
+++ Percy/Env.cs
@@ -42,7 +42,7 @@ namespace PercyIO.Appium
 
     internal static String GetClientInfo()
     {
-      return "percy-appium-dotnet/3.0.1-alpha.1";
+      return "percy-appium-dotnet/3.0.2";
     }
 
     internal static Boolean ForceFullPage()
