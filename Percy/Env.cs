5ab3746a9b36c391c521080860aaba71bf3f0193
this-is-shivamsingh <shivam.si@browserstack.com>
2024-02-06 15:30:11 +0530

    :bookmark: release-v3.0.3

--- Percy/Env.cs
+++ Percy/Env.cs
@@ -42,7 +42,7 @@ namespace PercyIO.Appium
 
     internal static String GetClientInfo()
     {
-      return "percy-appium-dotnet/3.0.3-beta.0";
+      return "percy-appium-dotnet/3.0.3";
     }
 
     internal static Boolean ForceFullPage()
