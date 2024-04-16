c120c65094f0ef4c8d50e1c61a899eafdd1d6a15
prklm10 <pradumraj360@gmail.com>
2024-04-16 15:20:32 +0530

    releasing v3.0.6

--- Percy/Env.cs
+++ Percy/Env.cs
@@ -42,7 +42,7 @@ namespace PercyIO.Appium
 
     internal static String GetClientInfo()
     {
-      return "percy-appium-dotnet/3.0.5";
+      return "percy-appium-dotnet/3.0.6";
     }
 
     internal static Boolean ForceFullPage()
